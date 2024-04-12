3d285f726101e9fc4bff66ccb817805cf28820a8
ACubero <alexcuar@gmail.com>
2024-04-12 21:41:54 +0200

    Conectar a supabase

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template
 import requests
+from supabase import create_client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+url = os.getenv('SUPABASE_URL')
+key = os.getenv('SUPABASE_KEY')
 
 app = Flask(__name__)
 
 @app.route('/')
 def mostrar_libros():
-    url_api = "https://api-libros-biblia.onrender.com/"
-    response = requests.get(url_api)
-    if response.status_code == 200:
-        libros = response.json()
-        print(libros)
-        return render_template('libros.html', libros=libros)
-    else:
+    supabase = create_client(url,key)
+    response = supabase.table("books").select("*").execute()
+    print(response)
+    if len(response.data) > 0:
+        return render_template('libros.html', libros=response.data)
+    else: 
         return "Error al obtener los libros"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
+
+
